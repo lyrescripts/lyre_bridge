d046aa4b4907d59ba2505fd47b7e73b3214b7dcb
gtolontop <cruckstore+github@gmail.com>
2026-05-01 16:26:43 +0200

    feat(qtarget): add resource config

---
+++ resources/qtarget/resource.lua
@@ -0,0 +1,18 @@
+LyreBridge.registerResource("qtarget", {
+    path = "resources/qtarget",
+    bridge = {
+        locked = false,
+        client = {
+            "bridge/client/client.lua",
+        },
+        server = {
+        },
+    },
+    sql = {
+        locked = false,
+        files = {
+        },
+        frameworkFiles = {
+        },
+    },
+})
