236c4b678a24130d4167adb114ca3b2cc2d0feea
gtolontop <cruckstore+github@gmail.com>
2026-05-01 16:20:09 +0200

    feat(drivingschool): add resource config

---
+++ resources/lyre_drivingschool/resource.lua
@@ -0,0 +1,25 @@
+LyreBridge.registerResource("lyre_drivingschool", {
+    path = "resources/lyre_drivingschool",
+    bridge = {
+        locked = false,
+        client = {
+            "bridge/client/cl_esx.lua",
+            "bridge/client/cl_example.lua",
+            "bridge/client/cl_qbcore.lua",
+        },
+        server = {
+            "bridge/server/sv_esx.lua",
+            "bridge/server/sv_example.lua",
+            "bridge/server/sv_qbcore.lua",
+        },
+    },
+    sql = {
+        locked = false,
+        files = {
+            { id = "import_sql", path = "sql/import.sql", required = true, order = 10 },
+        },
+        frameworkFiles = {
+        },
+    },
+})
+
