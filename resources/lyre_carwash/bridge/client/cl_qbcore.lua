280aaec6f79b73892a828ae04870435a49bcd203
gtolontop <cruckstore+github@gmail.com>
2026-05-01 16:18:19 +0200

    feat(carwash): add QBCore client bridge

---
+++ resources/lyre_carwash/bridge/client/cl_qbcore.lua
@@ -0,0 +1,29 @@
+_G.bridge = _G.bridge or {}
+
+local this = "QBCORE"
+
+_G.bridge[this] = {}
+
+_G.bridge[this].autoDetect = function()
+	return GetResourceState("qb-core") == "started"
+end
+
+local bridge = _G.bridge[this]
+
+--[[
+	BRIDGE FUNCTIONS
+]]
+
+---init
+---@return void
+---@public
+function bridge:init()
+	self.object = exports["qb-core"]:GetCoreObject()
+end
+
+---getSelfIdentifier
+---@return string
+---@public
+function bridge:getSelfIdentifier()
+	return bridge.object.Functions.GetPlayerData().citizenid
+end
