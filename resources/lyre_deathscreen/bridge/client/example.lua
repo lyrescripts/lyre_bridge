e88db1ef3b44bad7e769edcd63156d71c47a1c9f
gtolontop <cruckstore+github@gmail.com>
2026-05-01 16:19:45 +0200

    feat(deathscreen): add example client bridge

---
+++ resources/lyre_deathscreen/bridge/client/example.lua
@@ -0,0 +1,31 @@
+_G.bridge = _G.bridge or {}
+
+local this = "EXAMPLE"
+
+_G.bridge[this] = {}
+
+_G.bridge[this].autoDetect = function()
+	-- Return true when your framework/resource should use this bridge.
+	return false
+end
+
+local bridge = _G.bridge[this]
+
+--[[
+	BRIDGE FUNCTIONS
+]]
+
+---init
+---@return void
+---@public
+function bridge:init()
+	-- Initialize your framework object here when needed.
+end
+
+---revivePlayer
+---@return boolean
+---@public
+function bridge:revivePlayer()
+	-- Trigger your framework revive here and return true when it handled the revive.
+	return false
+end
