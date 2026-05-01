824512e2375168a9fd8e5c104c57547b75d7f74f
gtolontop <cruckstore+github@gmail.com>
2026-05-01 16:51:17 +0200

    feat(fuel): add getPlayerJob stub to example client bridge

--- resources/lyre_fuel/bridge/client/cl_example.lua
+++ resources/lyre_fuel/bridge/client/cl_example.lua
@@ -21,3 +21,11 @@ local bridge = _G.bridge[this]
 function bridge:init()
 	-- Customize this function, this function is executed when the bridge is detected. You can for example set self.object to the shared object of your framework.
 end
+
+---getPlayerJob
+---@description Gets the current player's job data
+---@return table|nil job The player job object, or nil if not available
+---@public
+function bridge:getPlayerJob()
+	-- Edit this function to match your framework's functions
+end
