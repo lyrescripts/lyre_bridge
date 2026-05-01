d2cba859b6858b084e536288aa868d4d3b5c44e6
gtolontop <cruckstore+github@gmail.com>
2026-05-01 16:18:17 +0200

    feat(carwash): add ESX client bridge

---
+++ resources/lyre_carwash/bridge/client/cl_esx.lua
@@ -0,0 +1,29 @@
+_G.bridge = _G.bridge or {}
+
+local this = "ESX"
+
+_G.bridge[this] = {}
+
+_G.bridge[this].autoDetect = function()
+	return GetResourceState("es_extended") == "started"
+end
+
+local bridge = _G.bridge[this]
+
+--[[
+	BRIDGE FUNCTIONS
+]]
+
+---init
+---@return void
+---@public
+function bridge:init()
+	self.object = exports["es_extended"]:getSharedObject()
+end
+
+---getSelfIdentifier
+---@return string
+---@public
+function bridge:getSelfIdentifier()
+	return bridge.object.GetPlayerData().identifier
+end
