c261e936fba517b31e8108d6ff9808433b0af3ea
gtolontop <cruckstore+github@gmail.com>
2026-05-01 16:26:38 +0200

    feat(ox_target): add resource config

---
+++ resources/ox_target/resource.lua
@@ -0,0 +1,18 @@
+LyreBridge.registerResource("ox_target", {
+    path = "resources/ox_target",
+    bridge = {
+        locked = false,
+        client = {
+            "bridge/client/client.lua",
+        },
+        server = {
+        },
+    },
+    sql = {
+        locked = false,
+        files = {
+        },
+        frameworkFiles = {
+        },
+    },
+})
