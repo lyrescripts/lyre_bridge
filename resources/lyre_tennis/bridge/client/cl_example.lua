cbc84a7bf6bc4de0652bf715161a3bb8d976c6c2
gtolontop <cruckstore+github@gmail.com>
2026-05-01 16:26:16 +0200

    feat(tennis): add example client bridge

---
+++ resources/lyre_tennis/bridge/client/cl_example.lua
@@ -0,0 +1,31 @@
+_G.bridge = _G.bridge or {}
+
+local this = "EXAMPLE"
+
+_G.bridge[this] = {}
+
+_G.bridge[this].autoDetect = function()
+	-- Customize this function
+	return false
+end
+
+local bridge = _G.bridge[this]
+
+--[[
+	BRIDGE FUNCTIONS
+]]
+
+---init
+---@return void
+---@public
+function bridge:init()
+	-- Customize this function, this function is executed when the bridge is detected. You can for example set self.object to the shared object of your framework.
+end
+
+---getPlayerName
+---@return string
+---@public
+function bridge:getPlayerName()
+	-- Edit this function to return the player's name according to your framework
+	return ""
+end
