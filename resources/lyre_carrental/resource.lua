ef70ac4b601f5d2fc1f20ed2128d3117998c7933
gtolontop <cruckstore+github@gmail.com>
2026-05-01 16:17:47 +0200

    feat(carrental): add resource config

---
+++ resources/lyre_carrental/resource.lua
@@ -0,0 +1,24 @@
+LyreBridge.registerResource("lyre_carrental", {
+    path = "resources/lyre_carrental",
+    bridge = {
+        locked = false,
+        client = {
+            "bridge/client/cl_esx.lua",
+            "bridge/client/cl_example.lua",
+            "bridge/client/cl_qbcore.lua",
+        },
+        server = {
+            "bridge/server/sv_esx.lua",
+            "bridge/server/sv_example.lua",
+            "bridge/server/sv_qbcore.lua",
+        },
+    },
+    sql = {
+        locked = false,
+        files = {
+        },
+        frameworkFiles = {
+        },
+    },
+})
+
