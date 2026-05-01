bb111f8c0bac0f80ccccb94bef0400179a1b9816
gtolontop <cruckstore+github@gmail.com>
2026-05-01 16:19:39 +0200

    feat(deathscreen): add QBox client bridge

---
+++ resources/lyre_deathscreen/bridge/client/qbox.lua
@@ -0,0 +1,34 @@
+_G.bridge = _G.bridge or {}
+
+local this = "QBOX"
+
+_G.bridge[this] = {}
+
+_G.bridge[this].autoDetect = function()
+	return GetResourceState("qbx_core") == "started"
+end
+
+local bridge = _G.bridge[this]
+
+--[[
+	BRIDGE FUNCTIONS
+]]
+
+---init
+---@return void
+---@public
+function bridge:init()
+	self.object = exports["qbx_core"]
+end
+
+---revivePlayer
+---@return boolean
+---@public
+function bridge:revivePlayer()
+	if GetResourceState("qbx_medical") == "started" then
+		TriggerEvent("qbx_medical:client:playerRevived")
+		return true
+	end
+
+	return false
+end
