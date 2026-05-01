c18ac634c423d9f16625f66c61b48c7f88ec20a2
gtolontop <cruckstore+github@gmail.com>
2026-05-01 16:21:59 +0200

    feat(garage): add resource config

---
+++ resources/lyre_garage/resource.lua
@@ -0,0 +1,33 @@
+LyreBridge.registerResource("lyre_garage", {
+    path = "resources/lyre_garage",
+    bridge = {
+        locked = false,
+        client = {
+            "bridge/client/cl_esx.lua",
+            "bridge/client/cl_example.lua",
+            "bridge/client/cl_qbcore.lua",
+        },
+        server = {
+            "bridge/server/sv_esx.lua",
+            "bridge/server/sv_example.lua",
+            "bridge/server/sv_qbcore.lua",
+        },
+    },
+    sql = {
+        locked = false,
+        files = {
+        },
+        frameworkFiles = {
+            ESX = {
+                { id = "import_esx", path = "sql/import_esx.sql", required = true, order = 10 },
+            },
+            QBCORE = {
+                { id = "import_qb", path = "sql/import_qb.sql", required = true, order = 10 },
+            },
+            QBOX = {
+                { id = "import_qb", path = "sql/import_qb.sql", required = true, order = 10 },
+            },
+        },
+    },
+})
+
