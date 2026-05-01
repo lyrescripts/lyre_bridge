3585312ea90c3790c1b2f0fbc75b779d73ed6919
gtolontop <cruckstore+github@gmail.com>
2026-05-01 16:24:09 +0200

    feat(illegalmissions-atm): add example server bridge

---
+++ resources/lyre_illegalmissions-atm/bridge/server/sv_example.lua
@@ -0,0 +1,23 @@
+_G.bridge = _G.bridge or {}
+
+local this = "EXAMPLE"
+
+_G.bridge[this] = {}
+
+_G.bridge[this].autoDetect = function()
+	-- Customize this function
+	return false
+end
+
+local bridge = _G.bridge[this]
+
+--[[
+	BRIDGE FUNCTIONS
+]]
+
+---init
+---@return void
+---@public
+function bridge:init()
+	-- Customize this function, this function is executed when the bridge is detected. You can for example set self.object to the shared object of your framework.
+end
