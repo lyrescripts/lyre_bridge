a2e4de422f4b6e2a5ef59edaf3d9e890cd1b7b27
gtolontop <cruckstore+github@gmail.com>
2026-05-01 16:18:24 +0200

    feat(carwash): add ESX server bridge

---
+++ resources/lyre_carwash/bridge/server/sv_esx.lua
@@ -0,0 +1,143 @@
+_G.bridge = _G.bridge or {}
+
+local this = "ESX"
+
+_G.bridge[this] = {}
+
+_G.bridge[this].autoDetect = function()
+	return GetResourceState("es_extended") == "started"
+end
+
+local bridge = _G.bridge[this]
+
+--[[
+	BRIDGE FUNCTIONS
+]]
+
+---init
+---@return void
+---@public
+function bridge:init()
+	self.object = exports["es_extended"]:getSharedObject()
+end
+
+---getPlayerFromId
+---@param playerId number
+---@return table
+---@public
+function bridge:getPlayerFromId(playerId)
+	local xPlayer = self.object.GetPlayerFromId(playerId)
+
+	if not xPlayer then
+		return false
+	end
+
+	local player = {}
+
+	player.getIdentifier = function()
+		return xPlayer.identifier
+	end
+
+	player.showNotification = function(message)
+		xPlayer.showNotification(message)
+	end
+
+	player.getAccount = function(account)
+		return xPlayer.getAccount(account).money
+	end
+
+	player.removeAccountMoney = function(account, amount)
+		xPlayer.removeAccountMoney(account, amount)
+	end
+
+	player.addAccountMoney = function(account, amount)
+		xPlayer.addAccountMoney(account, amount)
+	end
+
+	return player
+end
+
+---getIdFromIdentifier
+---@param identifier string
+---@return number
+---@public
+function bridge:getIdFromIdentifier(identifier)
+	if not identifier then
+		return false
+	end
+	local player = self.object.GetPlayerFromIdentifier(identifier)
+	if not player then
+		return false
+	end
+	return player.source
+end
+
+---updateOfflinePlayerAccount
+---@param identifier string
+---@param account string
+---@param amount number
+---@return void
+---@public
+function bridge:updateOfflinePlayerAccount(identifier, account, amount)
+	if not identifier then
+		return
+	end
+	if not account then
+		return
+	end
+	if not amount then
+		return
+	end
+
+	local response = MySQL.query.await("SELECT * FROM `users` WHERE `identifier` = ?", { identifier })
+	if #response == 0 then
+		return false
+	end
+
+	local accounts = json.decode(response[1].accounts)
+
+	if not accounts[account] then
+		return false
+	end
+
+	accounts[account] = accounts[account] + amount
+
+	local newAccounts = json.encode(accounts)
+
+	MySQL.query("UPDATE `users` SET `accounts` = @accounts WHERE `identifier` = @identifier", {
+		["@accounts"] = newAccounts,
+		["@identifier"] = identifier,
+	})
+end
+
+---expressRefillAction
+---@param stationId string
+---@param water number
+---@param soap number
+---@param wax number
+---@param maxWater number
+---@param maxSoap number
+---@param maxWax number
+---@param stocks table
+---@return void
+---@public
+function bridge:expressRefillAction(stationId, water, soap, wax, maxWater, maxSoap, maxWax, stocks)
+	-- Fill this function if you want to customize the express refill action
+	-- If you want to use this, you have to put the config Config.expressRefillAction to "custom"
+end
+
+---customRefillFunction
+---@param stationId string
+---@param water number
+---@param soap number
+---@param wax number
+---@param maxWater number
+---@param maxSoap number
+---@param maxWax number
+---@param stocks table
+---@return void
+---@public
+function bridge:customRefillFunction(stationId, water, soap, wax, maxWater, maxSoap, maxWax, stocks)
+	-- Fill this function if you want to customize the refill action
+	-- If you want to use this, you have to put the config Config.refillAction to "custom"
+end
