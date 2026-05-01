4fcfb834a3939fdf065d2a3e9ff99706159cdc24
gtolontop <cruckstore+github@gmail.com>
2026-05-01 16:19:34 +0200

    feat(deathscreen): add resource config

---
+++ resources/lyre_deathscreen/resource.lua
@@ -0,0 +1,28 @@
+LyreBridge.registerResource("lyre_deathscreen", {
+    path = "resources/lyre_deathscreen",
+    bridge = {
+        locked = false,
+        client = {
+            "bridge/client/esx.lua",
+            "bridge/client/example.lua",
+            "bridge/client/qbcore.lua",
+            "bridge/client/qbox.lua",
+            "bridge/client/standalone.lua",
+        },
+        server = {
+            "bridge/server/esx.lua",
+            "bridge/server/example.lua",
+            "bridge/server/qbcore.lua",
+            "bridge/server/qbox.lua",
+            "bridge/server/standalone.lua",
+        },
+    },
+    sql = {
+        locked = false,
+        files = {
+        },
+        frameworkFiles = {
+        },
+    },
+})
+
