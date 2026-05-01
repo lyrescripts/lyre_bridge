746da9dbe22c3dc551063da56feaff813a379a1f
gtolontop <cruckstore+github@gmail.com>
2026-05-01 16:18:04 +0200

    feat(carrental): add example server bridge

---
+++ resources/lyre_carrental/bridge/server/sv_example.lua
@@ -0,0 +1,50 @@
+_G.bridge = _G.bridge or {}
+
+local this = "EXAMPLE"
+
+_G.bridge[this] = {}
+
+_G.bridge[this].autoDetect = function()
+	-- Customize this function
+	return false
+end
+
+local bridge = _G.bridge[this]
+
+--[[
+	BRIDGE FUNCTIONS
+]]
+
+---init
+---@return void
+---@public
+function bridge:init()
+	-- Customize this function, this function is executed when the bridge is detected. You can for example set self.object to the shared object of your framework.
+end
+
+---getPlayerFromId
+---@param playerId number
+---@return table
+---@public
+function bridge:getPlayerFromId(playerId)
+	local player = {}
+
+	player.showNotification = function(message)
+		-- Customize this function
+	end
+
+	player.getAccount = function(account)
+		-- Customize this function
+		return 0
+	end
+
+	player.removeAccountMoney = function(account, amount)
+		-- Customize this function
+	end
+
+	player.addAccountMoney = function(account, amount)
+		-- Customize this function
+	end
+
+	return player
+end
