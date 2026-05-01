14914db6a3a245b24a6f101dd6d88502a0cdefc1
gtolontop <cruckstore+github@gmail.com>
2026-05-01 16:19:37 +0200

    feat(deathscreen): add QBCore client bridge

---
+++ resources/lyre_deathscreen/bridge/client/qbcore.lua
@@ -0,0 +1,48 @@
+_G.bridge = _G.bridge or {}
+
+local this = "QBCORE"
+
+_G.bridge[this] = {}
+
+_G.bridge[this].autoDetect = function()
+	return GetResourceState("qb-core") == "started"
+end
+
+local bridge = _G.bridge[this]
+
+--[[
+	BRIDGE FUNCTIONS
+]]
+
+---init
+---@return void
+---@public
+function bridge:init()
+	self.object = exports["qb-core"]:GetCoreObject()
+end
+
+---revivePlayer
+---@return boolean
+---@public
+function bridge:revivePlayer()
+	local handled = false
+
+	if GetResourceState("qb-ambulancejob") == "started" then
+		local success = pcall(function()
+			exports["qb-ambulancejob"]:RevivePlayer(Config.respawn.health)
+		end)
+
+		if success then
+			handled = true
+		end
+	end
+
+	if GetResourceState("hospital") == "started" or GetResourceState("qb-ambulancejob") == "started" then
+		TriggerEvent("hospital:client:Revive")
+		TriggerServerEvent("hospital:server:SetDeathStatus", false)
+		TriggerServerEvent("hospital:server:SetLaststandStatus", false)
+		return true
+	end
+
+	return handled
+end
