2f68f9bcf6d2b1b68a7cd4531861d47c056daf1b
gtolontop <cruckstore+github@gmail.com>
2026-05-01 16:23:47 +0200

    feat(illegalmissions): add SQL import

---
+++ resources/lyre_illegalmissions/sql/import.sql
@@ -0,0 +1,12 @@
+CREATE TABLE lyre_illegalmissions_datas (
+    `identifier` VARCHAR(99) PRIMARY KEY,
+    `reputation` INT,
+    `stat-wins` INT,
+    `stat-losses` INT,
+    `stat-solo` INT,
+    `stat-team` INT,
+    `stat-winstreak` INT,
+    `stat-maxwinstreak` INT,
+    `stat-totalmoney` INT,
+    `stat-maxrepuation` INT
+);
