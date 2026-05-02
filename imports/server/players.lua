bbaf9aa72fdc226ae044c45f2561900c2c5b7527
gtolontop <cruckstore+github@gmail.com>
2026-05-03 01:58:05 +0200

    refactor: slim server players import entrypoint

---
+++ imports/server/players.lua
@@ -0,0 +1,7 @@
+local Core = LyreBridge
+
+Core.loadImport("imports/server/players/helpers.lua")
+Core.loadImport("imports/server/players/esx.lua")
+Core.loadImport("imports/server/players/qbcore.lua")
+Core.loadImport("imports/server/players/qbox.lua")
+Core.loadImport("imports/server/players/module.lua")
