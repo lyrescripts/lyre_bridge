7d563499a4dd2a80f19973a598311f0cf446336f
gtolontop <cruckstore+github@gmail.com>
2026-05-03 01:58:11 +0200

    docs: add server qbox bridge example

---
+++ custom/server/qbox_bridge.lua
@@ -0,0 +1,13 @@
+-- Example: add a custom Qbox server bridge candidate.
+--[[
+_G.bridge = _G.bridge or {}
+_G.bridge.QBOX = _G.bridge.QBOX or {}
+
+function _G.bridge.QBOX.autoDetect()
+    return GetResourceState("qbx_core") == "started"
+end
+
+function _G.bridge.QBOX:init()
+    self.object = exports["qbx_core"]
+end
+]]
