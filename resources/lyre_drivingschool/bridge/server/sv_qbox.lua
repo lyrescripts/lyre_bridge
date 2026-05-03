7106b91b8e34d575a1da4bb6cef908d55f2f93f5
gtolontop <cruckstore+github@gmail.com>
2026-05-03 02:37:41 +0200

    feat(qbox): add lyre_drivingschool server bridge

---
+++ resources/lyre_drivingschool/bridge/server/sv_qbox.lua
@@ -0,0 +1,95 @@
+_G.bridge = _G.bridge or {}
+
+local this = "QBOX"
+
+_G.bridge[this] = {}
+
+_G.bridge[this].autoDetect = function()
+	return GetResourceState("qbx_core") == "started"
+end
+
+local bridge = _G.bridge[this]
+
+--[[
+	BRIDGE FUNCTIONS
+]]
+
+---init
+---@return void
+---@public
+function bridge:init()
+	self.object = exports["qbx_core"]
+end
+
+---hasLicense
+---Checks if a player has a specific license
+---@param source number The player source
+---@param licenseType string The type of license (car, motorcycle, truck)
+---@param cb function Callback function with result (true/false)
+---@return void
+---@public
+function bridge:hasLicense(source, licenseType, cb)
+	local Player = self.object:GetPlayer(source)
+	if not Player then
+		cb(false)
+		return
+	end
+
+	-- License mapping for Qbox
+	local licenseMap = {
+		["car"] = "driver",
+		["motorcycle"] = "motorcycle",
+		["truck"] = "truck",
+	}
+
+	local license = licenseMap[licenseType]
+	if not license then
+		cb(false)
+		return
+	end
+
+	-- Check if player has the license in metadata
+	local metadata = Player.PlayerData.metadata
+	local hasLicense = metadata.licences and metadata.licences[license] == true
+	cb(hasLicense)
+end
+
+---grantLicense
+---Grants a driving license to the player
+---@param source number The player source
+---@param licenseType string The type of license (car, motorcycle, truck)
+---@return boolean success Whether the grant was successful
+---@public
+function bridge:grantLicense(source, licenseType)
+	local Player = self.object:GetPlayer(source)
+	if not Player then
+		return false
+	end
+
+	-- License mapping for Qbox
+	local licenseMap = {
+		["car"] = "driver",
+		["motorcycle"] = "motorcycle",
+		["truck"] = "truck",
+	}
+
+	local license = licenseMap[licenseType]
+	if not license then
+		return false
+	end
+
+	-- Check if player already has the license
+	local metadata = Player.PlayerData.metadata
+	if metadata.licences and metadata.licences[license] then
+		return true -- Already has license
+	end
+
+	-- Grant the license
+	if not metadata.licences then
+		metadata.licences = {}
+	end
+	metadata.licences[license] = true
+	Player.Functions.SetMetaData("licences", metadata.licences)
+	Player.Functions.Save()
+	return true
+end
