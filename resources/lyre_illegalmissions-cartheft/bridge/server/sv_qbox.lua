e4b38579f2a496346e7de7d70cde12380bfdf769
gtolontop <cruckstore+github@gmail.com>
2026-05-03 02:37:45 +0200

    feat(qbox): add lyre_illegalmissions-cartheft server bridge

---
+++ resources/lyre_illegalmissions-cartheft/bridge/server/sv_qbox.lua
@@ -0,0 +1,35 @@
+_G.bridge = _G.bridge or {}
+
+local this = "QBOX"
+
+_G.bridge[this] = {}
+
+_G.bridge[this].autoDetect = function()
+	return GetResourceState("qbx_core") == "started"
+end
+
+local bridge = _G.bridge[this]
+
+--[[
+	BRIDGE FUNCTIONS
+]]
+
+---init
+---@return void
+---@public
+function bridge:init()
+	self.object = exports["qbx_core"]
+end
+
+---unlockVehicle
+---@param source number
+---@param vehicle table
+---@return void
+---@public
+function bridge:unlockVehicle(source, vehicle)
+	local vehicle = NetworkGetEntityFromNetworkId(vehicle)
+	if not DoesEntityExist(vehicle) then
+		return
+	end
+	SetVehicleDoorsLocked(vehicle, 1)
+end
