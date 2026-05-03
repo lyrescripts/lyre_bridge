bb246d5e3446cc77196e766f49b106b7cf5728a3
gtolontop <cruckstore+github@gmail.com>
2026-05-03 02:37:49 +0200

    feat(qbox): add lyre_tennis client bridge

---
+++ resources/lyre_tennis/bridge/client/cl_qbox.lua
@@ -0,0 +1,38 @@
+_G.bridge = _G.bridge or {}
+
+local this = "QBOX"
+
+_G.bridge[this] = {}
+
+_G.bridge[this].autoDetect = function()
+	return GetResourceState("qbx_core") == "started"
+end
+
+local bridge = _G.bridge[this]
+
+--[[
+	BRIDGE FUNCTIONS
+]]
+
+---init
+---@return void
+---@public
+function bridge:init()
+	self.object = exports["qbx_core"]
+end
+
+---getPlayerName
+---@return string
+---@public
+function bridge:getPlayerName()
+	if not self.object then
+		return "Unknown"
+	end
+	local playerData = self.object:GetPlayerData()
+	if not playerData or not playerData.charinfo then
+		return "Unknown"
+	end
+	local firstname = playerData.charinfo.firstname or ""
+	local lastname = playerData.charinfo.lastname or ""
+	return firstname .. " " .. lastname
+end
