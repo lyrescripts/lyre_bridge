957af67f048a9efaf25b8d2d10ccaacb70bb831f
gtolontop <cruckstore+github@gmail.com>
2026-05-09 01:45:26 +0200

    refactor(qb-target): normalize bridge registry

--- resources/qb-target/resource.lua
+++ resources/qb-target/resource.lua
@@ -1,16 +1,1 @@
-LyreBridge.registerResource("qb-target", {
-    path = "resources/qb-target",
-    bridge = {
-        client = {
-            "bridge/client/client.lua",
-        },
-        server = {
-        },
-    },
-    sql = {
-        files = {
-        },
-        frameworkFiles = {
-        },
-    },
-})
+LyreBridge.registerResource("qb-target")
