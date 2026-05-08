fab83f3e050b4da89c206801f213285ed08bd2eb
gtolontop <cruckstore+github@gmail.com>
2026-05-09 01:44:37 +0200

    feat(bridge): export active client bridge info

--- client/main.lua
+++ client/main.lua
@@ -2,6 +2,10 @@ exports("BridgeVersion", function()
     return LyreBridge.versionString()
 end)
 
+exports("GetActiveBridgeInfo", function(resourceName, side)
+    return LyreBridge.getActiveBridgeInfo(resourceName, side)
+end)
+
 CreateThread(function()
     Wait(0)
     LyreBridge.log("debug", "Client core ready.", {
