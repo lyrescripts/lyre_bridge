e1199091a3266d020f0e90eaa32245a1b1d5cd02
gtolontop <cruckstore+github@gmail.com>
2026-05-09 02:23:02 +0200

    docs(illegalmissions-cartheft): describe DLC

--- resources/lyre_illegalmissions-cartheft/resource.lua
+++ resources/lyre_illegalmissions-cartheft/resource.lua
@@ -1,1 +1,8 @@
-LyreBridge.registerResource("lyre_illegalmissions-cartheft")
+LyreBridge.registerResource("lyre_illegalmissions-cartheft", {
+	metadata = {
+		group = "illegalmissions",
+		role = "dlc",
+		parent = "lyre_illegalmissions",
+		mission = "cartheft",
+	},
+})
