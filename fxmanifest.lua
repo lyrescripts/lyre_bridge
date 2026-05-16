a394c50bff3baae12c7a343116cc8c66898fbaef
Epyi <68242948+epyidev@users.noreply.github.com>
2026-05-16 17:44:06 +0200

    chore(release): bump version to 2.1.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game("gta5")
 
 author("Lyre Scripts")
 description("Provider-based compatibility bridge for the Lyre resource pack.")
-version("2.0.0")
+version("2.1.0")
 lua54("yes")
 
 escrow_ignore({
